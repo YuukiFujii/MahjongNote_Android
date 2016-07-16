4b51095047abdec7abc74327be053524ec908ca3
Yuki Fujii <yukfujii@yahoo-corp.jp>
2016-07-17 01:52:15 +0900

    結果画面のレイアウト作成とadmobの実装

--- app/src/main/java/com/ravious/mahjongnote/MainActivity.java
+++ app/src/main/java/com/ravious/mahjongnote/MainActivity.java
@@ -2,6 +2,8 @@ package com.ravious.mahjongnote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+
     }
 }
